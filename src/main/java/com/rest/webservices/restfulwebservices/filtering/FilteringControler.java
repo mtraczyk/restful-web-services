e55810e27949d18b2c04ee90273a26a128c757d2
mtraczyk <michaltraczyk943@gmail.com>
2022-12-07 22:09:40 +0100

    Refactor of FilteringController

--- src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringControler.java
+++ src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringControler.java
@@ -1,16 +1,13 @@
 package com.rest.webservices.restfulwebservices.filtering;
 
 import com.fasterxml.jackson.databind.ser.FilterProvider;
-import com.fasterxml.jackson.databind.ser.PropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.List;
 
 @RestController
 public class FilteringControler {
