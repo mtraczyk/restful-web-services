5735bcc4f3e6036bfe46d5bddec2b4c677f760e8
mtraczyk <michaltraczyk943@gmail.com>
2022-12-07 22:43:19 +0100

    H2 connection and some test data

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -3,11 +3,15 @@ package com.rest.webservices.restfulwebservices.user;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
-	
+
+	@Id
 	private Integer id;
 	
 	@Size(min = 2, max = 30, message = "Name should have at least 2 characters")
@@ -24,6 +28,8 @@ public class User {
 		this.name = name;
 		this.birthDate = birthDate;
 	}
+
+	public User() {}
 
 	public Integer getId() {
 		return id;
